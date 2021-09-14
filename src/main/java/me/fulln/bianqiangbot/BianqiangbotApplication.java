5e3e7b521d9759d84a0d74fef3aada8f1aa99866
fulln <245193247@qq.com>
2021-09-14 14:17:40 +0800

    Initial commit

---
+++ src/main/java/me/fulln/bianqiangbot/BianqiangbotApplication.java
@@ -0,0 +1,13 @@
+package me.fulln.bianqiangbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianqiangbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BianqiangbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/fulln/bianqiangbot/BianqiangbotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fulln.bianqiangbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BianqiangbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
